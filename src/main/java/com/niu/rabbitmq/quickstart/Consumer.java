b38a5026066b0f824b484e7344d5a1d0296d7ad1
Nza6920 <1484663282@qq.com>
2020-07-03 21:15:03 +0800

    init project

---
+++ src/main/java/com/niu/rabbitmq/quickstart/Consumer.java
@@ -0,0 +1,10 @@
+package com.niu.rabbitmq.quickstart;
+
+/**
+ * 消费者
+ * @Description: TODO
+ * @Author nza
+ * @Date 2020/7/3
+ **/
+public class Consumer {
+}
---
+++ src/main/java/com/niu/rabbitmq/quickstart/Producer.java
@@ -0,0 +1,10 @@
+package com.niu.rabbitmq.quickstart;
+
+/**
+ * 生产者
+ * @Description: TODO
+ * @Author nza
+ * @Date 2020/7/3
+ **/
+public class Producer {
+}
