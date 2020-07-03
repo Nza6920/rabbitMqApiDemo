773a0965b8def5b6738867428d9373b1add7d0be
Nza6920 <1484663282@qq.com>
2020-07-03 23:10:49 +0800

    quick start

--- src/main/java/com/niu/rabbitmq/quickstart/Consumer.java
+++ src/main/java/com/niu/rabbitmq/quickstart/Consumer.java
@@ -1,10 +1,49 @@
 package com.niu.rabbitmq.quickstart;
 
+import com.rabbitmq.client.*;
+
 /**
  * 消费者
+ *
  * @Description: TODO
  * @Author nza
  * @Date 2020/7/3
  **/
 public class Consumer {
+
+    public static void main(String[] args) throws Exception {
+        // 1.创建一个ConnectionFactory, 并进行配置
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("106.15.121.10");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+
+        // 2.通过连接工厂创建连接
+        Connection conn = connectionFactory.newConnection();
+
+        // 3.通过conn创建一个Channel
+        Channel channel = conn.createChannel();
+
+        // 4.创建一个队列
+        String queueName = "test001";
+        channel.queueDeclare(queueName, true, false, false, null);
+
+        // 5.创建消费者
+        QueueingConsumer queueingConsumer = new QueueingConsumer(channel);
+
+        // 6.设置channel
+        channel.basicConsume(queueName, true, queueingConsumer);
+
+        // 7.获取消息
+        while (true) {
+
+            QueueingConsumer.Delivery delivery = queueingConsumer.nextDelivery();
+
+            String msg = new String(delivery.getBody());
+            System.out.println("消费端: " + msg);
+
+            Envelope envelope = delivery.getEnvelope();
+//            envelope.getDeliveryTag()
+        }
+    }
 }
--- src/main/java/com/niu/rabbitmq/quickstart/Producer.java
+++ src/main/java/com/niu/rabbitmq/quickstart/Producer.java
@@ -1,5 +1,9 @@
 package com.niu.rabbitmq.quickstart;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
 /**
  * 生产者
  * @Description: TODO
@@ -7,4 +11,34 @@ package com.niu.rabbitmq.quickstart;
  * @Date 2020/7/3
  **/
 public class Producer {
+
+    public static void main(String[] args) throws Exception {
+        // 1.创建一个ConnectionFactory, 并进行配置
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("106.15.121.10");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+
+        // 2.通过连接工厂创建连接
+        Connection conn = connectionFactory.newConnection();
+
+        // 3.通过conn创建一个Channel
+        Channel channel = conn.createChannel();
+
+        System.out.println("开始发送");
+
+        // 4.通过channel发送数据
+        for (int i = 0; i < 5; i++) {
+            String msg = "Hello rabbitmq " + i;
+
+            // 1.exchange 2.routingKey
+            channel.basicPublish("", "test001", null, msg.getBytes());
+        }
+
+        // 5.关闭连接
+        channel.close();
+        conn.close();
+
+        System.out.println("发送完毕");
+    }
 }
