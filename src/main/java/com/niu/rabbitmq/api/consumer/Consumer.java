076d70d0758c56d0bdf6a282e5d94865c15099ac
Nza6920 <1484663282@qq.com>
2020-07-05 23:11:56 +0800

    添加 自定义消费端 Demo

---
+++ src/main/java/com/niu/rabbitmq/api/consumer/Consumer.java
@@ -0,0 +1,37 @@
+package com.niu.rabbitmq.api.consumer;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 自定义 consumer
+ * @Description: TODO
+ * @Author nza
+ * @Date 2020/7/5
+ **/
+public class Consumer {
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("106.15.121.10");
+        factory.setPort(5672);
+        factory.setVirtualHost("/");
+
+        Connection conn = factory.newConnection();
+
+        Channel channel = conn.createChannel();
+
+        String exchangeName = "test_consumer_exchange";
+        String routingKey = "consumer.#";
+        String queueName = "test_consumer_queue";
+
+        channel.exchangeDeclare(exchangeName, "topic", true, false, null);
+        channel.queueDeclare(queueName, true, false, false, null);
+        channel.queueBind(queueName, exchangeName, routingKey);
+        channel.basicConsume(queueName, true, new MyConsumer(channel));
+    }
+}
